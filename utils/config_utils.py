dac851f3e8c523b300ddf2ba49025a2d9fe09c6b
zhangrui-wolf <zhangrui_wolf@163.com>
2020-11-20 10:41:02 +0800

    重构代码，完善日志功能

--- utils/config_utils.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-
-import yaml
-
-
-# 读取配置文件
-def load_config(config_path):
-    with open(config_path, encoding='utf8') as f:
-        return yaml.load(f, Loader=yaml.FullLoader)
